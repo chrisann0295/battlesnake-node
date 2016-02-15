14e0f27f177fa7fd4c32339657a037bb1d864e3e
= <=>
2016-02-14 16:36:23 -0800

    partially implemented current game states

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,21 @@ var config  = require('../config.json');
 var express = require('express');
 var router  = express.Router();
 
+//global snake info
+var total_score = 0;
+
+//map holds state of all running games (keys are the gameid)
+var games = {};
+
+//example game state
+var game = {
+  id: "example";
+  state: "alive";
+  coords: [[0,0]];
+  score: 0;
+}
+games["example"] = game;
+
 // Get the state of the snake
 router.get(config.routes.state, function (req, res) {
   // Do something here to calculate the returned state
@@ -17,15 +32,27 @@ router.get(config.routes.state, function (req, res) {
     score: 4
   };
 
-  return res.json(data);
+	//return all game states
+  return res.json(games);
 });
 
 // Start
 router.post(config.routes.start, function (req, res) {
-  // Do something here to start the game
-  // Hint: do something with the incoming game_id? ;)
-  console.log('Game ID:', req.body.game_id);
 
+  //log the game id
+  console.log('Game ID:', req.body.game_id);   
+
+  //create new game state
+  games[req.body.game_id] = {
+    id:req.body.game_id,
+    state: "alive",
+    coords: [],
+    score:0;
+  }
+
+	games[req.body.game_id].width = req.body.width;
+  games[req.body.game_id].height = req.body.height;
+ 
   // Response data
   var data = {
     name: config.snake.name,
@@ -41,18 +68,43 @@ router.post(config.routes.start, function (req, res) {
 router.post(config.routes.move, function (req, res) {
   // Do something here to generate your move
 
-  // Response data
-  var data = {
-    move: 'up', // one of: ["up", "down", "left", "right"]
-    taunt: 'What?!' || config.snake.taunt.move
-  };
+  var gameid = req.body.game_id;
+  var currState = games[gameid];
 
-  return res.json(data);
+  //catch games that have ended/haven't begun
+  if (currState === "null" || currState === "undefined"){
+    res.status(404);
+    res.end();
+    return;
+
+  }else{
+  	
+		var board = req.body.board;
+		var turn = req.body.turn;
+		var snakes = req.body.snakes;
+		var food = req.body.food;
+
+
+
+
+		// Response data
+		var data = {
+		  move: 'up', // one of: ["up", "down", "left", "right"]
+		  taunt: 'What?!' || config.snake.taunt.move
+		};
+
+		return res.json(data);
+  }
 });
 
 // End the session
 router.post(config.routes.end, function (req, res) {
-  // Do something here to end your snake's session
+ 
+  var gameid = req.body.game_id;
+  //update global score
+  total_score += games[gameid].score;
+  //delete game state from map
+  games[gameid] = null;
 
   // We don't need a response so just send back a 200
   res.status(200);
