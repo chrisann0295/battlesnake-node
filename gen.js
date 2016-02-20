0cd279fec4011f8e915e58d8d1132b4f46c7e9d9
= <=>
2016-02-20 14:22:37 -0800

    importing array gen module

---
+++ gen.js
@@ -0,0 +1,32 @@
+exports.genArray = function(h,w,snakes,food,walls,coin){
+	var arr = [h][w]; //0,0 to h-1,w-1
+	var health = 100; //TODO call health()
+	var food = (100-health)*2 
+	vals = {"snake":-200, "wall":-300, "coin":100, "food":food}
+	
+	for(i=0; i<food.length;i++){
+		coords = food[i]		
+		arr[coords[0]][coords[1]] = vals['food']
+	}
+
+	for(i=0; i<snakes.length;i++){
+		asnake = snakes[i]
+		for(j=0; j<asnake.length;j++){
+			coords = asnake[i][j]		
+			arr[coords[0]][coords[1]] = vals['snake']
+		}
+	}
+
+	for(i=0; i<walls.length;i++){
+		coords = walls[i]		
+		arr[coords[0]][coords[1]] = vals['wall']
+	}
+
+	for(i=0; i<gold.length;i++){
+		coords = gold[i]		
+		arr[coords[0]][coords[1]] = vals['coin']
+	}
+	return arr
+
+}
+
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ var config = require('../config.json');
 var express = require('express');
 var router = express.Router();
 var Cell = require('../cell.js')
+var Gen = require('../gen.js')
 
 //global snake info
 var total_score = 0;
@@ -72,37 +73,6 @@ router.post(config.routes.start, function(req, res) {
 });
 
 
-function genArray(h,w,snakes,food,walls,coin){
-	var arr = [h][w]; //0,0 to h-1,w-1
-	var health = 100; //TODO call health()
-	var food = (100-health)*2 
-	vals = {"snake":-200, "wall":-300, "coin":100, "food":food}
-	
-	for(i=0; i<food.length;i++){
-		coords = food[i]		
-		arr[coords[0]][coords[1]] = vals['food']
-	}
-
-	for(i=0; i<snakes.length;i++){
-		asnake = snakes[i]
-		for(j=0; j<asnake.length;j++){
-			coords = asnake[i][j]		
-			arr[coords[0]][coords[1]] = vals['snake']
-		}
-	}
-
-	for(i=0; i<walls.length;i++){
-		coords = walls[i]		
-		arr[coords[0]][coords[1]] = vals['wall']
-	}
-
-	for(i=0; i<gold.length;i++){
-		coords = gold[i]		
-		arr[coords[0]][coords[1]] = vals['coin']
-	}
-	return arr
-
-}
 
 // Move
 router.post(config.routes.move, function(req, res) {
@@ -127,7 +97,7 @@ router.post(config.routes.move, function(req, res) {
 		var walls = req.body.walls;
 		var gold = req.body.gold; 
 
-		var arr = genArr(currState.height,currState.width,,snake,food,walls,gold);
+		var arr = Gen.genArr(currState.height,currState.width,,snake,food,walls,gold);
 
 		console.log(arr)
  	
